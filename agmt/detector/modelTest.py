d7fd376b9ee65c4e14c7e5b78d8c899cabb805f7
Pandiarajan D <pandiarajanyadhav10@gmail.com>
2025-11-17 23:50:38 +0530

    Update modelTest.py

--- agmt/detector/modelTest.py
+++ agmt/detector/modelTest.py
@@ -6,9 +6,8 @@ import cv2
 
 
 def fun(image_path):
-    #image_path = "C:/Users/pandi/OneDrive/Desktop/SEP\demo1/agmt/detector/TomatoHealthy2.jpg"
-    #model=tf.keras.models.load_model("C:/Users/Sankar/sep/agmt/detector/static/images/trained_model.keras")
-    model=tf.keras.models.load_model("C:/Users/pandi/OneDrive - SSN-Institute/SEP_Project/demo1/agmt/detector/static/images/trained_model.keras")
+    #image_path = "Any Plant Leaf Path if you want to test locally"
+    model=tf.keras.models.load_model("./static/images/trained_model.keras")
     image=tf.keras.preprocessing.image.load_img(image_path,target_size=(128,128))
     input_arr=tf.keras.preprocessing.image.img_to_array(image)
     input_arr=np.array([input_arr])
