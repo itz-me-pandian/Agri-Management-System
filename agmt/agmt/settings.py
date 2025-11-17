5bfacfe513fa5b372d0a3b1109f2e8e40ab6dfb1
Pandiarajan D <pandiarajanyadhav10@gmail.com>
2025-11-17 23:32:30 +0530

    Update settings.py

--- agmt/agmt/settings.py
+++ agmt/agmt/settings.py
@@ -134,7 +134,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='alloteasyregofficial@gmail.com'
-EMAIL_HOST_PASSWORD='qkxh hiha ohfl tcgw ' #'weiq onvg kbpt jnki '
+EMAIL_HOST_PASSWORD=APP PASSWORD
 EMAIL_USE_TLS=True
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 
